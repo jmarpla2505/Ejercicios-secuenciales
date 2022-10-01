203b80abef7fbccfd565d51179ff068abc1aa466
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-01 15:32:04 +0200

    Ejercicio 2 definitivo

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,5 +2,6 @@
 base = 0
 altura = 0
 base = (int)(input("Dime la base del rectángulo: "))
-altura = (int)(input("Dime la base del rectángulo: "))
-print("El área es " base*altura )
+altura = (int)(input("Dime la altura del rectángulo: "))
+print("El área es ", base*altura )
+print("El perímetro es ", 2*base+2*altura )
