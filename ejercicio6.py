3f1e3c39844236f020e04eb63beb3c68f633475d
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-02 19:51:19 +0200

    Terminado ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+#Calcular la media de tres números pedidos por teclado.
+
+numero1= 0
+numero2 = 0
+numero3 = 0
+
+numero1 = (int)(input("Dime un número:\n"))
+numero2 = (int)(input("Dime otro número:\n"))
+numero3 = (int)(input("Dime otro número:\n"))
+print("La media de los números es:", (numero1+numero2+numero3)/3)
