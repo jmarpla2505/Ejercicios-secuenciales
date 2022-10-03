f45e9c65a5a20062d9e1a9b1d8d0dfa0c1668cc1
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-03 11:13:31 +0200

    Terminado ejercicio 9

---
+++ ejercicio9.py
@@ -0,0 +1,7 @@
+#Una tienda ofrece un descuento del 15% sobre el total de la compra y un cliente desea saber cuanto deberá pagar finalmente por su compra.
+
+compratotal = 0
+compradescuento = 0
+compratotal = int(input("Dime el total de la compra:\n"))
+compradescuento= compratotal*0.85
+print("La compra final es", compradescuento)
