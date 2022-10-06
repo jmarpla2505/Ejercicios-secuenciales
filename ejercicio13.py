2ffc3c2810548d1bed38b51f29cb5b5bb5757a5f
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-06 10:10:06 +0200

    Terminado ejercicio 13

---
+++ ejercicio13.py
@@ -0,0 +1,6 @@
+#Realizar un algoritmos que lea un número y que muestre su raíz cuadrada y su
+#raíz cúbica.
+import math
+num = 0
+num = int(input("Dime un número:\n"))
+print("La raíz cuadrada es", math.sqrt(num), "y la raíz cúbica es", num**(1/3))
