97f1468740700eb566f254cc98dd8a88526d9423
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-06 22:03:07 +0200

    Terminado ej 20

--- ejercicio20.py
+++ ejercicio20.py
@@ -9,15 +9,15 @@ monedas1 = 0
 monedas50 = 0
 monedas20 = 0
 monedas10 = 0
-cents = 0
-centimoseuros = 0
+centimos = 0
+euros = 0
 
 monedas2 = int(input("Dime cúantas monedas tienes de 2 euros:\n"))
 monedas1 = int(input("Dime cúantas monedas tienes de 1 euro:\n"))
 monedas50 = int(input("Dime cúantas monedas tienes de 50 céntimos:\n"))
 monedas20 = int(input("Dime cúantas monedas tienes de 20 céntimos:\n"))
 monedas10 = int(input("Dime cúantas monedas tienes de 10 céntimos:\n"))
-cents = monedas50*50+monedas20*20+monedas10*10
-if cents>=100:
-    centimoseuros=abs(100-cents)
-    print("Tienes", int(monedas2*2+monedas1+centimoseuros/100), "euros y", monedas50*50+monedas20*20+monedas10*10-centimoseuros, "céntimos")
+euros = monedas2+monedas1
+centimos = monedas50*0.5+monedas20*0.2+monedas10*0.10
+dinerototal = euros+centimos
+print("En total, tienes:", dinerototal, "euros")
