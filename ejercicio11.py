d7a60222c2df776c6a8254574905cec3a2cd74e9
José Ángel <al_jmartosa@iessierradelasvillas.org>
2022-10-06 10:09:41 +0200

    Terminado ejercicio 11

---
+++ ejercicio11.py
@@ -0,0 +1,9 @@
+#Pide al usuario dos números y muestra la “distancia” entre ellos (el valor absoluto
+#de su diferencia, de modo que el resultado sea siempre positivo).
+
+num1 = 0
+num2 = 0
+
+num1 = int(input("Dime un número:\n"))
+num2 = int(input("Dime otro número:\n"))
+print("La distancia es",abs(num1-num2))
